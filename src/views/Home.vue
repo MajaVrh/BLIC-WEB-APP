aac836a87ae7062a78e52ee1fb69e1f423831740
MajaVrh <majavrh5@gmail.com>
2022-04-01 09:47:06 +0200

    prvi dio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div
+      class="card"
+      v-for="knjiga in knjige"
+      :key="knjiga.isbn"
+      @click="odvediMe(knjiga.isbn)"
+    >
+      <div>{{ knjiga.name }}</div>
+      <div>{{ knjiga.authors }}</div>
+      <div>{{ knjiga.released }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: 'Home',
+  name: "Home",
+
+  data() {
+    return {
+      knjige: [],
+    };
+  },
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  async mounted() {
+    await this.fetchKnjige();
+  },
+  methods: {
+    async fetchKnjige() {
+      const pomocni = await axios.get(
+        "https://www.anapioficeandfire.com/api/books"
+      );
+      const data = pomocni.data;
+      console.log(data);
+      this.knjige = data;
+    },
+    odvediMe(id) {
+      this.$router.push({ name: "knjiga", parms: { idRoutera: id } });
+    },
+  },
+};
 </script>
+
+<style scoped>
+.card {
+  background-color: yellow;
+  margin: 3rem;
+}
+</style>
---
+++ src/views/knjiga.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="knjiga" v-for="k in knjiga" :key="k.isbn"></div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "knjiga",
+  data() {
+    return {
+      knjiga: {},
+    };
+  },
+  methods: {
+    async fetchKnjiga() {
+      const id = this.$route.params.id;
+      const pomocni = await axios.get(
+        `https://www.anapioficeandfire.com/api/books/${id}`
+      );
+      this.knjiga = pomocni.data;
+    },
+  },
+};
+</script>
